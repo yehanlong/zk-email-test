afa49c36e0c99a65e887b1ec25ec7260db008eb5
yehanlong <952292896@qq.com>
2024-06-26 17:18:02 +0800

    update

--- packages/install-rust-circim-snarkjs.sh
+++ packages/install-rust-circim-snarkjs.sh
@@ -7,30 +7,30 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-install_rust() {
-        # Use expect to automate interactive selection
-        expect <<EOF
-            spawn curl -k --proto '=https' https://sh.rustup.rs -sSf | sh
-            expect "1) Proceed with standard installation (default - just press enter)"
-            send -- "1\r"
-            expect eof
-EOF
-}
+# install_rust() {
+#         # Use expect to automate interactive selection
+#         expect <<EOF
+#             spawn curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+#             expect "1) Proceed with standard installation (default - just press enter)"
+#             send -- "1\r"
+#             expect eof
+# EOF
+# }
 
 # Install expect if not already installed
-if ! command_exists expect; then
-    echo "Installing expect..."
-    sudo apt-get update
-    sudo apt-get install -y expect
-else
-    echo "expect is already installed."
-fi
+# if ! command_exists expect; then
+#     echo "Installing expect..."
+#     sudo apt-get update
+#     sudo apt-get install -y expect
+# else
+#     echo "expect is already installed."
+# fi
 
 # Install Rust via rustup if not already installed
 if ! command_exists rustup; then
     echo "Installing Rust via rustup..."
     # export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
-    install_rust
+    curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
     source $HOME/.cargo/env
 else
     echo "Rust is already installed."
